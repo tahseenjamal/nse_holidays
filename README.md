19946c95c0b386e6ae3b1c87f205e97a3aaddab4
Tahseen Jamal <tahseen.jamal@gmail.com>
2019-09-02 00:16:37 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,8 @@
+Using pip or conda, install below dependencies in your virtual environment
+
+bs4
+requests
+
+
+Enjoy!!
+Tahseen Jamal
--- nse_holidays.py
+++ nse_holidays.py
@@ -1,3 +1,7 @@
+# Coded by: Tahseen Jamal
+# Github: github.com/tahseenjamal
+# tahseen(dot)jamal(at)gmail.com
+
 from dateutil.parser import parse
 from datetime import datetime
 import bs4, requests
@@ -18,9 +22,12 @@ def holidays():
 
             return False
 
+    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
+
     url = 'https://www.nseindia.com/products/content/equities/equities/mrkt_timing_holidays.htm'
 
-    data = requests.get(url)
+    data = requests.get(url, headers=headers)
+
     html = bs4.BeautifulSoup(data.content, 'lxml')
     tables = html.find("table", { "class" : "holiday_list" })
     rows = tables.findAll('td')
