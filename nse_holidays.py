27918f80059ce45468faba03e4151e02dc198622
Tahseen Jamal <tahseen.jamal@gmail.com>
2023-12-11 14:17:34 +0400

    Update nse_holidays.py

--- nse_holidays.py
+++ nse_holidays.py
@@ -2,69 +2,45 @@
 # Github: github.com/tahseenjamal
 # tahseen(dot)jamal(at)gmail.com
 
-from dateutil.parser import parse
 from datetime import datetime, date
 from selenium import webdriver
-import bs4, os
+import os
 
-def trading_day():
 
-    holidays = []
+def get_nse_holidays:
 
-    def parse_date(string):
-        try:
-
-            holiday = datetime.strptime(string, "%d-%b-%Y").date()
-
-            return holiday
-
-        except ValueError:
-
-            return False
-
-    url = 'https://www1.nseindia.com/products/content/equities/equities/mrkt_timing_holidays.htm'
-
+    url = 'https://www.nseindia.com/resources/exchange-communication-holidays'
+    
     os.environ['MOZ_HEADLESS'] = '1'
-
+    
     driver = webdriver.Firefox()
-
+    
     driver.get(url)
-
+    
     page_content = driver.page_source
-
+    
     os.environ['MOZ_HEADLESS'] = '0'
-
+    
     driver.close()
+    
+    df = pd.read_html(page_content, attrs={'id': 'holidayTable'})[0]
+    holidays = df.Date.apply(lambda x : parse_date(x)).to_list()
 
-    html = bs4.BeautifulSoup(page_content, 'lxml')
-    tables = html.find("table", { "class" : "holiday_list" })
-    rows = tables.findAll('td')
+    return holidays
 
-    string_array = []
+def is_today_a_trading_day(holidays):
 
-    for row in rows:
-
-        string_array.extend(row.text.splitlines())
-
-    for string in string_array:
-
-        valid = parse_date(string)
-
-        if not valid:
-
-            continue
-
-        holidays.append(valid)
-
-    print(holidays)
-
+    # If today is in weekends or in the holiday dates then return true
     if date.today().weekday in [6,7] or date.today() in holidays:
-
+        return True
+    else:
         return False
 
-    return True
 
-print(trading_day())
+holidays = get_nse_holidays()
+
+print(is_today_a_trading_day(holidays))
+
 
 
 
